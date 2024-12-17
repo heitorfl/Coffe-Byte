8ac9c03ae350e6c60a22aaf40ac2a2e1d2043b13
Heitor Lima <141742426+heitorfl@users.noreply.github.com>
2024-12-16 22:42:40 -0300

    Início da estilização dos cards

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,9 @@ const button = document.querySelector("btn");
 const modal = document.querySelector("dialog");
 
 function mostrar(){
-    modal.classList.add("active")
     modal.showModal();
 }
 
 function fechar(){
-    modal.classList.remove("active")
     modal.close(); 
 }
